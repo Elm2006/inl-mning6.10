ccc5f4bbecfa9df099cd7be17837b159c57859a8
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-21 16:34:53 +0100

    Add project files.

---
+++ inlämning6.10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string svar = Console.ReadLine();
+        Console.WriteLine("Det finns "+AntalTallText(svar)+" stycken tal i den meningen");
+    }
+    static int AntalTallText(string text)
+    {
+        int gånger =0;
+        string[] ord = text.Split(' ');
+        for (int i = 0; i < ord.Length; i++)
+        {
+            try
+            {
+                double test = double.Parse(ord[i]);
+                gånger++;
+            }
+            catch
+            {
+
+            }
+        }
+        return gånger;
+    }
+}
